879d35422c038dd9f6e1d6a0addd015e4c0a621e
@alpha <sekyeredennis3@gmail.com>
2024-09-09 17:29:32 +0000

    new edit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi there! I just edited this code.'));
+app.get('/', (req, res) => res.send('Hi there! I just edited this code. Hopefully, it was a good one :)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
